c814ce2588af8aaffadfa0e527b3248920e535e2
Anthony Guo <anthony.guo@some.ox.ac.uk>
2015-03-01 10:01:29 +0000

    init db

--- notifista/init_database.sh
+++ notifista/init_database.sh
@@ -1,12 +1,9 @@
-curl -b cookies.txt -c cookies.txt --data "name=event1&password=asdfasdf"  http://localhost:1337/api/event  
-curl -b cookies.txt -c cookies.txt --data "name=ch1"  http://localhost:1337/api/event/channel  
-curl -b cookies.txt -c cookies.txt --data "name=ch2"  http://localhost:1337/api/event/channel  
-curl -b cookies.txt -c cookies.txt --data "event-name=event1&channel-name=ch2&message=msg1"  http://localhost:1337/api/message  
-curl -b cookies.txt -c cookies.txt --data "event-name=event1&channel-name=ch2&message=msg2"  http://localhost:1337/api/message  
-curl -b cookies.txt -c cookies.txt --data "event-name=event1&channel-name=ch2&message=msg3"  http://localhost:1337/api/message  
-curl -b cookies.txt -c cookies.txt --data "event-name=event1&channel-name=ch1&message=msg1"  http://localhost:1337/api/message  
-curl -b cookies.txt -c cookies.txt --data "event-name=event1&channel-name=ch1&message=msg2"  http://localhost:1337/api/message  
-curl -b cookies.txt -c cookies.txt --data "event-name=event1&channel-name=ch1&message=msg3"  http://localhost:1337/api/message  
+curl -b cookies.txt -c cookies.txt --data "name=HackLondon&password=asdfasdf"  http://localhost:1337/api/event  
+curl -b cookies.txt -c cookies.txt --data "name=food"  http://localhost:1337/api/event/channel  
+curl -b cookies.txt -c cookies.txt --data "name=general"  http://localhost:1337/api/event/channel  
+curl -b cookies.txt -c cookies.txt --data "event-name=HackLondon&channel-name=food&message=We're ordering more pizza soon!"  http://localhost:1337/api/message  
+curl -b cookies.txt -c cookies.txt --data "event-name=HackLondon&channel-name=food&message=Ready for some beer?"  http://localhost:1337/api/message  
+curl -b cookies.txt -c cookies.txt --data "event-name=HackLondon&channel-name=general&message=Happy hacking!"  http://localhost:1337/api/message  
 curl -b cookies.txt -c cookies.txt --data "email=lukas@me.com&password=asdfasdf" http://localhost:1337/api/user  
-curl --data "event-name=event1&channel-name=ch1" http://localhost:1337/api/user/channel -b cookies.txt -c cookies.txt
-curl --data "event-name=event1&channel-name=ch2" http://localhost:1337/api/user/channel -b cookies.txt -c cookies.txt
+curl --data "event-name=HackLondon&channel-name=food" http://localhost:1337/api/user/channel -b cookies.txt -c cookies.txt
+curl --data "event-name=HackLondon&channel-name=general" http://localhost:1337/api/user/channel -b cookies.txt -c cookies.txt
